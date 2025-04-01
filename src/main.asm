55288c14a2917ad1d973a44a58f502d786df4aec
robotsnh <88983536+meowpu@users.noreply.github.com>
2025-03-31 19:39:37 -0700

    done

---
+++ src/main.asm
@@ -0,0 +1,66 @@
+org 0x7C00
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+    
+
+main:
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+    hlt
+
+.halt:
+    jmp .halt
+
+
+
+msg_hello: db 'אחד באפריל! That should be Achad BeAfril, or April Fools, but BIOS cannot print Hebrew or apostrophes. BIOS stands for Basic Input Output System, and basically lets you say stuff in x86 assembly.', ENDL, 0
+;"Achad B'Afril!"
+
+times 510-($-$$) db 0
+dw 0AA55h
